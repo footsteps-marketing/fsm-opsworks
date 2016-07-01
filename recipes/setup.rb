cc5beed60733c4338879c1298771927e6ceda3c9
Aaron Hipple <aaron@footstepsmarketing.com>
2016-07-01 16:34:09 -0600

    New format for setup script

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -4,24 +4,17 @@
 #
 # Copyright (c) 2016 FootSteps Marketing, All Rights Reserved.
 
-package "Install nginx" do
-    package_name "nginx"
-end
-
-package "Install php" do
-    package_name "php-fpm"
-end
-
-package "Install php-mysql" do
-    package_name "php-mysql"
-end
-
-package "Install php-mcrypt" do
-    package_name "php-mcrypt"
-end
-
-package "Install letsencrypt" do
-    package_name "letsencrypt"
+[
+    'nginx',
+    'php-fpm',
+    'php-mysql',
+    'php-mcrypt',
+    'php7.0-xml',
+    'letsencrypt'
+].each do |installPackage|
+    package "Install #{installPackage}" do
+        package_name installPackage
+    end
 end
 
 # Handle a bug with kswapd0 eating lots of CPU
