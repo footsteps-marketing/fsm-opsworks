384544c1e86a116481469b0a15df044b9a55f996
Aaron Hipple <aaron@footstepsmarketing.com>
2016-07-01 11:47:03 -0600

    Debuggy thing

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -3,3 +3,10 @@
 # Recipe:: configure
 #
 # Copyright (c) 2016 FootSteps Marketing, All Rights Reserved.
+
+template "/etc/nginx/snippets/wordfence.conf" do
+    source "wordfence.conf.erb"
+    mode 0644
+    owner "root"
+    group "root"
+end
--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -7,7 +7,7 @@
 command = search('aws_opsworks_command').first
 
 search("aws_opsworks_app").each do |app|
-    if deploy === false
+    if app['deploy'] === false
         next
     end
 
@@ -15,7 +15,11 @@ search("aws_opsworks_app").each do |app|
     Chef::Log.info("***** Deploying #{app['shortname']} at #{command['sent_at']} ")
     current_revision = command['sent_at']
 
-    if app['app_source']['type'] == 'git'
-
-    end
+    # if app['app_source']['type'] == 'git'
+    #     git "#{deploy_root}" do
+    #         revision app['app_source']['revision'] 
+    #         repository app['app_source']['url'] 
+    #         action :sync
+    #     end
+    # end
 end
