1575141d11afbe86f768a372aa48896ac34f3e16
Aaron Hipple <aaron@footstepsmarketing.com>
2016-07-01 12:04:16 -0600

    Now try actually deploying the thing

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -13,13 +13,42 @@ search("aws_opsworks_app").each do |app|
 
     current_revision = command['sent_at'].delete("^0-9")
     deploy_root = "/srv/www/#{app['shortname']}/#{current_revision}"
-    Chef::Log.info("***** Deploying #{app['shortname']} at #{command['sent_at']} to #{deploy_root}")
+    Chef::Log.info("**************** Deploying #{app['shortname']} to #{deploy_root}")
 
-    # if app['app_source']['type'] == 'git'
-    #     git "#{deploy_root}" do
-    #         revision app['app_source']['revision'] 
-    #         repository app['app_source']['url'] 
-    #         action :sync
-    #     end
+    if app['app_source']['type'] == 'git'
+        key_path = "/var/keys/#{app['shortname']}_rsa"
+
+        if app['app_source']['ssh_key'] != 'null'
+            file "#{key_path}" do
+                owner "root"
+                group "root"
+                mode "0600"
+                content "#{app['app_source']['ssh_key']}"
+            end
+        end
+
+        git "#{deploy_root}" do
+            revision app['app_source']['revision'] 
+            repository app['app_source']['url'] 
+            if app['app_source']['ssh_key'] != 'null'
+                ssh_wrapper "ssh -i #{key_path}"
+            end
+            action :sync
+        end
+    end
+    
+    # template "#{deploy_root}/wp-config.php" do
+    #     source "wp-config.php.erb"
+    #     mode 0660
+    #     group 'www-data'
+    #     owner "www-data"
+    #     
+    #     variables(
+    #         :database   => (deploy[:database][:database] rescue nil),
+    #         :user       => (deploy[:database][:username] rescue nil),
+    #         :password   => (deploy[:database][:password] rescue nil),
+    #         :host       => (deploy[:database][:host] rescue nil),
+    #         :keys       => (keys rescue nil)
+    #     )
     # end
 end
