4351cf8e903be4d1e94fd4b44fc547268a299a0f
Aaron Hipple <aaron@footstepsmarketing.com>
2016-07-01 10:41:58 -0600

    Add setup recipe

---
+++ recipes/setup.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: fsm-opsworks
+# Recipe:: setup
+#
+# Copyright (c) 2016 FootSteps Marketing, All Rights Reserved.
+
+package "Install nginx" do
+    package_name "nginx"
+end
+
+package "Install php" do
+    package_name "php-fpm"
+end
+
+package "Install php-mysql" do
+    package_name "php-mysql"
+end
+
+package "Install php-mcrypt" do
+    package_name "php-mcrypt"
+end
