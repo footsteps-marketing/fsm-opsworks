e70bc2cde72f4aa52819673f326e97ba2c686ea2
Aaron Hipple <aaron@footstepsmarketing.com>
2016-09-16 13:33:26 -0600

    Performance improvements (gzip)

--- fsm-opsworks/recipes/setup-production.rb
+++ fsm-opsworks/recipes/setup-production.rb
@@ -7,51 +7,14 @@
 # 
 # Install some image processing libraries
 # 
-[
-    'letsencrypt',
-    'build-essential',
-    'git',
-    'autoconf',
-    'pkg-config',
-    'nasm',
-    'libtool'
-].each do |installPackage|
-    package "#{installPackage}" do
-        package_name installPackage
-    end
-end
-
-script "install_mozjpeg" do
-    action :nothing
-    subscribes :run, 'package[build-essential]', :delayed
-    interpreter "bash"
-    cwd "/tmp"
+bash "handle_kswapd0_bug" do
     user "root"
     code <<-EOH
-        if [ ! -f /opt/mozjpeg/bin/cjpeg ]; then
-            sudo apt-get install -y build-essential autoconf pkg-config nasm libtool
-            git clone https://github.com/mozilla/mozjpeg.git
-            cd mozjpeg
-            autoreconf -fiv
-            ./configure --with-jpeg8
-            make
-            sudo make install
-        fi
-    EOH
+        touch /etc/udev/rules.d/40-vm-hotadd.rules
+        EOH
+    notifies :request_reboot, 'reboot[required]', :delayed
 end
 
-script "install_jpegarchive" do
+reboot 'required' do
     action :nothing
-    subscribes :run, 'package[build-essential]', :delayed
-    interpreter "bash"
-    cwd "/tmp"
-    user "root"
-    code <<-EOH
-        if [ ! -f /usr/local/bin/jpeg-recompress ]; then
-            git clone https://github.com/danielgtaylor/jpeg-archive.git
-            cd jpeg-archive
-            make
-            sudo make install
-        fi
-    EOH
 end
