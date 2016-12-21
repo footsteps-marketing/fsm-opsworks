51ce8c43dbc955c63eb8bedb8232837a72fd4f26
Aaron Hipple <aaron@footstepsmarketing.com>
2016-12-21 09:22:12 -0700

    Syntax update in configure-cloudwatch-logs

--- fsm-opsworks/recipes/configure-cloudwatch-logs.rb
+++ fsm-opsworks/recipes/configure-cloudwatch-logs.rb
@@ -4,6 +4,10 @@
 #
 # Copyright (c) 2016 FootSteps Marketing, All Rights Reserved.
 
+apps = search("aws_opsworks_app")
+stack = search("aws_opsworks_stack").first
+instance = search("aws_opsworks_instance").first
+
 template "/tmp/cwlogs.cfg" do
   source "tmp/cwlogs.cfg.erb"
   owner "root"
@@ -11,8 +15,8 @@ template "/tmp/cwlogs.cfg" do
   mode 0644
   variables(
     :logbase => '/var/log/nginx',
-    :apps => (search("aws_opsworks_app") rescue nil),
-    :stack => (search("aws_opsworks_stack").first rescue nil)
-    :instance => (search("aws_opsworks_instance").first rescue nil)
+    :apps => (apps rescue nil),
+    :stack => (stack rescue nil)
+    :instance => (instance rescue nil)
   )
 end
