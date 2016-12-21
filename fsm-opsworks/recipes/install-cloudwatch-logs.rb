7d7e545a3af44a5aae5185d6a999789746c7ebaa
Aaron Hipple <aaron@footstepsmarketing.com>
2016-12-21 10:51:53 -0700

    Properly notify actions in install-cloudwatch-logs

--- fsm-opsworks/recipes/install-cloudwatch-logs.rb
+++ fsm-opsworks/recipes/install-cloudwatch-logs.rb
@@ -6,14 +6,18 @@
 
 directory "/opt/aws/cloudwatch" do
   recursive true
+  notifies :create, 'remote_file[/opt/aws/cloudwatch/awslogs-agent-setup.py]', :immediately
 end
 
 remote_file "/opt/aws/cloudwatch/awslogs-agent-setup.py" do
+  action :nothing
   source "https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py"
+  notifies :run, 'execute[Install CloudWatch Logs agent]', :immediately
   mode "0755"
 end
  
 execute "Install CloudWatch Logs agent" do
+  action :nothing
   command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r #{node[:cwlogs][:region]} -c /tmp/cwlogs.cfg"
   notifies :run, 'execute[Start CloudWatch Logs agent]', :immediately
   not_if { system "pgrep -f aws-logs-agent-setup" }
