24ae956f6c84fe56e1cc08b60eafc209d766e06c
Aaron Hipple <aaron@footstepsmarketing.com>
2016-12-21 11:06:00 -0700

    change execute block names

--- fsm-opsworks/recipes/install-cloudwatch-logs.rb
+++ fsm-opsworks/recipes/install-cloudwatch-logs.rb
@@ -10,18 +10,18 @@ end
 
 remote_file "/opt/aws/cloudwatch/awslogs-agent-setup.py" do
   source "https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py"
-  notifies :run, 'execute[Install CloudWatch Logs agent]', :immediately
+  notifies :run, 'execute[install_cflogs_agent]', :immediately
   mode "0755"
 end
  
-execute "Install CloudWatch Logs agent" do
+execute "install_cflogs_agent" do
   action :nothing
   command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r #{node[:cwlogs][:region]} -c /tmp/cwlogs.cfg"
-  notifies :run, 'execute[Start CloudWatch Logs agent]', :immediately
+  notifies :run, 'execute[start_cflogs_agent]', :immediately
   not_if { system "pgrep -f aws-logs-agent-setup" }
 end
 
-execute "Start CloudWatch Logs agent" do
+execute "start_cflogs_agent" do
   action :nothing
   command "systemctl enable awslogs; systemctl restart awslogs"
 end
