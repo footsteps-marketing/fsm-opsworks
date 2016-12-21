9e310f6e80016910b895bbc7116112c8f0753686
Aaron Hipple <aaron@footstepsmarketing.com>
2016-12-21 11:18:35 -0700

    Subscribe instead of notify?

--- fsm-opsworks/recipes/install-cloudwatch-logs.rb
+++ fsm-opsworks/recipes/install-cloudwatch-logs.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: fsm-opsworks
-# Recipe:: configure-cloudwatch-logs
+# Recipe:: install-cloudwatch-logs
 #
 # Copyright (c) 2016 FootSteps Marketing, All Rights Reserved.
 
@@ -10,20 +10,20 @@ end
 
 remote_file "/opt/aws/cloudwatch/awslogs-agent-setup.py" do
   source "https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py"
-  notifies :run, 'execute[install_cflogs_agent]', :immediately
   mode "0755"
 end
  
 execute "install_cflogs_agent" do
   action :nothing
   command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r #{node[:cwlogs][:region]} -c /tmp/cwlogs.cfg"
+  subscribes :run, 'remote_file[/opt/aws/cloudwatch/awslogs-agent-setup.py]', :immediately
   notifies :write, 'log[install_cflogs_agent]', :immediately
-  notifies :run, 'execute[start_cflogs_agent]', :immediately
   not_if { system "pgrep -f aws-logs-agent-setup" }
 end
 
 execute "start_cflogs_agent" do
   action :nothing
+  subscribes :run, 'execute[install_cflogs_agent]', :immediately
   notifies :write, 'log[start_cflogs_agent]', :immediately
   command "systemctl enable awslogs; systemctl restart awslogs"
 end
