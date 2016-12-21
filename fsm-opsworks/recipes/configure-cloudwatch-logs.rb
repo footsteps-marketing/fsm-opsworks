e156ab7ca4fafc06a36cd5a776742f20329c0283
Aaron Hipple <aaron@footstepsmarketing.com>
2016-12-20 17:03:53 -0700

    Add CloudWatch logging

---
+++ fsm-opsworks/recipes/configure-cloudwatch-logs.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: fsm-opsworks
+# Recipe:: configure-cloudwatch-logs
+#
+# Copyright (c) 2016 FootSteps Marketing, All Rights Reserved.
+
+template "/tmp/cwlogs.cfg" do
+  source "tmp/cwlogs.cfg.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(
+    :logbase => '/var/log/nginx',
+    :apps => (search("aws_opsworks_app") rescue nil),
+    :stack => (search("aws_opsworks_stack").first rescue nil)
+    :instance => (search("aws_opsworks_instance").first rescue nil)
+  )
+end
