0c7ca672230c038f44141469bc69305e81c84156
Stephen King <stephen@footstepsmarketing.com>
2017-03-16 16:27:22 -0600

    Actually run the damn command...

--- fsm-opsworks/libraries/domains.rb
+++ fsm-opsworks/libraries/domains.rb
@@ -2,6 +2,7 @@ module Domains
     def self.get(app_root, app_domain)
         command = "php #{app_root}/get-mapped-domains.php"
         shell = Mixlib::ShellOut.new(command, :user => 'root')
+        result = shell.run_command
         
         Chef::Log.info("**************** DOMAINS RESULT: #{shell.stdout}")
         Chef::Log.info("**************** DOMAINS ERROR:  #{shell.stderr}")
