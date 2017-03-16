036a5d1b9e3e654f0c377b563c81926b0930c459
Stephen King <stephen@footstepsmarketing.com>
2017-03-16 14:35:06 -0600

    Include ShellOut mixing at Domains class root level

--- fsm-opsworks/libraries/domains.rb
+++ fsm-opsworks/libraries/domains.rb
@@ -1,7 +1,7 @@
 class Chef::Recipe::Domains
+    include Chef::Mixin::ShellOut
+    
     def self.get(app_root, app_domain)
-        include Chef::Mixin::ShellOut
-        
         command = "php #{app_root}/get-mapped-domains.php"
         domains = self.shell_out(command).stdout.split("\n")
         domains.unshift("#{app_domain}")
