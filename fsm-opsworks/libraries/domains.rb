f63b3864d8b0a7be1e3588541862ad22c2568ca3
Stephen King <stephen@footstepsmarketing.com>
2017-03-16 16:15:24 -0600

    Try using Mixlib::ShellOut

--- fsm-opsworks/libraries/domains.rb
+++ fsm-opsworks/libraries/domains.rb
@@ -1,12 +1,11 @@
 module Domains
     def self.get(app_root, app_domain)
-        shell = Chef::ShellOut.new(:user => 'root')
         command = "php #{app_root}/get-mapped-domains.php"
-        result = shell.shell_out(command)
+        shell = Mixlib::ShellOut.new(command, :user => 'root')
         
-        Chef::Log.info("**************** DOMAINS RESULT: #{result.stdout}")
-        Chef::Log.info("**************** DOMAINS ERROR:  #{result.stderr}")
-        domains = result.stdout.split("\n")
+        Chef::Log.info("**************** DOMAINS RESULT: #{shell.stdout}")
+        Chef::Log.info("**************** DOMAINS ERROR:  #{shell.stderr}")
+        domains = shell.stdout.split("\n")
         domains.unshift("#{app_domain}")
 
         return domains
