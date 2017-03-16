e83bab88167911fff717ea5202fbd2dae14704c6
Stephen King <stephen@footstepsmarketing.com>
2017-03-16 15:57:47 -0600

    Move domain conf block to after domain getter script block

--- fsm-opsworks/recipes/deploy.rb
+++ fsm-opsworks/recipes/deploy.rb
@@ -273,37 +273,6 @@ search("aws_opsworks_app").each do |app|
         end
     end
 
-    #
-    # Now write out the domain confs...
-    # 
-    Chef::Log.info("**************** MAPPING DOMAINS")
-    Domains.get(deploy_root, node[:wordpress][:multisite][:domain_current_site]) do |domains|
-        domains.each do |domain|
-            Chef::Log.info("***** Mapping Domain: #{domain}")
-            template "/etc/nginx/sites-available/#{domain}.conf" do
-                source "etc/nginx/sites-available/SITE.conf.erb"
-                mode 0644
-                owner "root"
-                group "root"
-
-                variables(
-                    :server_root => server_root,
-                    :logbase => '/var/log/nginx',
-                    :app => (app rescue nil),
-                    :url => (domain rescue nil),
-                    :ssl => node[:letsencrypt][:get_certificates]
-                )
-            end
-
-            link "/etc/nginx/sites-enabled/#{domain}.conf" do
-                to "/etc/nginx/sites-available/#{domain}.conf"
-                if node[:letsencrypt][:get_certificates] == false
-                    notifies :restart, "service[nginx]", :delayed
-                end
-            end
-        end
-    end
-
 
     # Get or generate salts
     if node['wordpress']['salt'] == false then
@@ -363,6 +332,37 @@ search("aws_opsworks_app").each do |app|
         mode 0700
         group "root"
         owner "root"
+    end
+
+    #
+    # Now write out the domain confs...
+    # 
+    Chef::Log.info("**************** MAPPING DOMAINS")
+    Domains.get(deploy_root, node[:wordpress][:multisite][:domain_current_site]) do |domains|
+        domains.each do |domain|
+            Chef::Log.info("***** Mapping Domain: #{domain}")
+            template "/etc/nginx/sites-available/#{domain}.conf" do
+                source "etc/nginx/sites-available/SITE.conf.erb"
+                mode 0644
+                owner "root"
+                group "root"
+
+                variables(
+                    :server_root => server_root,
+                    :logbase => '/var/log/nginx',
+                    :app => (app rescue nil),
+                    :url => (domain rescue nil),
+                    :ssl => node[:letsencrypt][:get_certificates]
+                )
+            end
+
+            link "/etc/nginx/sites-enabled/#{domain}.conf" do
+                to "/etc/nginx/sites-available/#{domain}.conf"
+                if node[:letsencrypt][:get_certificates] == false
+                    notifies :restart, "service[nginx]", :delayed
+                end
+            end
+        end
     end
 
 
