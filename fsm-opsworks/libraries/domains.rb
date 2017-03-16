4e7a503ead6d008f5bcd4b8c43de8972b5a55007
Stephen King <stephen@footstepsmarketing.com>
2017-03-16 15:25:25 -0600

    Make Domains.get a self method

--- fsm-opsworks/libraries/domains.rb
+++ fsm-opsworks/libraries/domains.rb
@@ -1,7 +1,7 @@
 module Domains
     include Chef::Mixin::ShellOut
     
-    def get(app_root, app_domain)
+    def self.get(app_root, app_domain)
         command = "php #{app_root}/get-mapped-domains.php"
         result = self.shell_out(command)
         
