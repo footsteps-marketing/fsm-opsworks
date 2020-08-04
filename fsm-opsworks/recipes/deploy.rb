df3330c9c9113ea343d84c5e620aea482c82b888
Jon Childs <jon.g.childs@gmail.com>
2020-08-03 19:11:56 -0600

    Add back wordfence-waf.php to recipe

--- fsm-opsworks/recipes/deploy.rb
+++ fsm-opsworks/recipes/deploy.rb
@@ -406,7 +406,6 @@ search("aws_opsworks_app").each do |app|
         group deploy_group
     end
 
-
     # Write our lil' domain getter script out
     template "#{deploy_root}/get-mapped-domains.php" do
         action :nothing
@@ -418,14 +417,14 @@ search("aws_opsworks_app").each do |app|
     end
 
     # Write out wordfence-waf.php
-    # template "#{deploy_root}/wordpress/wordfence-waf.php" do
-    #     action :nothing
-    #     subscribes :create, "template[#{deploy_root}/wordpress/wp-config.php]", :immediately
-    #     source "srv/www/APPNAME/RELEASETIME/wordfence-waf.php.erb"
-    #     mode 0640
-    #     owner deploy_user
-    #     group deploy_group
-    # end
+    template "#{deploy_root}/wordpress/wordfence-waf.php" do
+        action :nothing
+        subscribes :create, "template[#{deploy_root}/wordpress/wp-config.php]", :immediately
+        source "srv/www/APPNAME/RELEASETIME/wordfence-waf.php.erb"
+        mode 0640
+        owner deploy_user
+        group deploy_group
+    end
 
 
     # Clean up any excluded plugins and themes
